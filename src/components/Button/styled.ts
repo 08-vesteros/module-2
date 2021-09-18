d02a91723ba7af541883510520f1cce3d034916a
rklebede <RLebedev@stream.ru>
2021-09-18 21:46:38 +0300

    fix after review

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ButtonWrapper = styled.div`
-  color: red;
+	color: red;
 `;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -26,4 +26,4 @@ module.exports = {
 			template: './public/index.html',
 		}),
 	],
-}
+};
