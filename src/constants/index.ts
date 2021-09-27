03de7af53112b2c415debc4f693af88fd3bd31b0
rklebede <RLebedev@stream.ru>
2021-09-27 19:25:25 +0300

    first game verstion

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+// game settings
+export const CANVAS_WIDTH = 600;
+export const CANVAS_HEIGHT = 150;
+export const GRAVITY = 5;
+export const JUPM_HEIGHT = 30;
+export const GAME_SPEED = 15;
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,46 +1,96 @@
 import React, { useEffect, useRef } from 'react';
+import {
+	CANVAS_WIDTH,
+	JUPM_HEIGHT,
+	CANVAS_HEIGHT,
+	GRAVITY,
+	GAME_SPEED,
+} from '../../constants';
 import { Canvas, Wrapper } from './styled';
-
-const WIDTH = 600;
-const HEIGHt = 150;
-const GRAVITY = 15;
+import { ObstaclesType } from './types';
 
 const Game = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const xPos = 0;
-	let yPos = 0;
+	let yPos = 200;
+	const obstacles: ObstaclesType[] = [];
+	obstacles[0] = {
+		x: CANVAS_WIDTH * 2,
+		y: 220,
+		w: 50,
+		h: 80,
+	};
 
-	const dyno = (ctx: any, x: number, y: number, w: number, h: number) => {
+	const createDyno = (
+		ctx: CanvasRenderingContext2D,
+		x: number,
+		y: number,
+		w: number,
+		h: number
+	) => {
 		ctx.beginPath();
 		ctx.fillRect(x, y, w, h);
 		ctx.closePath();
 	};
 
-	const jump = (e: React.KeyboardEvent) => {
-		if (yPos === 200 && e.keyCode === 32) {
-			yPos = 50;
+	const jump = () => {
+		// TOTO smooth jumping up
+		if (yPos === 200) {
+			yPos = JUPM_HEIGHT;
 		}
+	};
+
+	const createObstacle = (
+		ctx: CanvasRenderingContext2D,
+		x: number,
+		y: number,
+		w: number,
+		h: number
+	) => {
+		ctx.beginPath();
+		ctx.fillRect(x, y, w, h);
+		ctx.closePath();
 	};
 
 	useEffect(() => {
 		const canvas = canvasRef?.current;
-		const context = canvas?.getContext('2d');
+		const context = canvas?.getContext('2d') as CanvasRenderingContext2D;
 		if (canvas) {
-			canvas.width = WIDTH * 2;
-			canvas.height = HEIGHt * 2;
-			canvas.style.width = `${WIDTH}px`;
-			canvas.style.height = `${HEIGHt}px`;
+			canvas.width = CANVAS_WIDTH * 2;
+			canvas.height = CANVAS_HEIGHT * 2;
+			canvas.style.width = `${CANVAS_WIDTH}px`;
+			canvas.style.height = `${CANVAS_HEIGHT}px`;
 		}
 
 		const render = () => {
 			if (canvas) {
 				context?.clearRect(0, 0, canvas.width, canvas.height);
-				dyno(context, xPos, yPos, 100, 100);
+				createDyno(context, xPos, yPos, 100, 100);
 
 				if (yPos < 200) {
 					yPos += GRAVITY;
 				} else {
 					yPos = 200;
+				}
+
+				for (let i = 0; i < obstacles.length; i += 1) {
+					createObstacle(
+						context,
+						obstacles[i].x,
+						obstacles[i].y,
+						obstacles[i].w,
+						obstacles[i].h
+					);
+					obstacles[i].x -= GAME_SPEED;
+					// TODO random show obstacles
+					if (obstacles[i].x === 135) {
+						obstacles.push({
+							x: canvas.width,
+							y: 220,
+							w: 50,
+							h: 80,
+						});
+					}
 				}
 			}
 			requestAnimationFrame(render);
