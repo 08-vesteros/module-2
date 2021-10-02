b3ec3c371505da48c7ba0ed24bd3afc5ae6f4209
rklebede <RLebedev@stream.ru>
2021-10-02 18:02:57 +0300

    add start screen

--- src/pages/game/styled.ts
+++ src/pages/game/styled.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-	width: 100%;
-	height: 700px;
-`;
 export const Canvas = styled.canvas`
 	border: 1px solid red;
 	outline: none;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import Forum from '../pages/forum';
+import Game from '../pages/game';
 import Home from '../pages/home';
 import Profile from '../pages/profile';
 import Scoreboard from '../pages/scoreboard';
 import Signin from '../pages/signin';
 import Signup from '../pages/signup';
+import Start from '../pages/start';
 import { TRoute } from './types';
 
 export const routes: Array<TRoute> = [
@@ -22,6 +24,14 @@ export const routes: Array<TRoute> = [
 	{
 		path: '/profile',
 		component: Profile,
+	},
+	{
+		path: '/start',
+		component: Start,
+	},
+	{
+		path: '/game',
+		component: Game,
 	},
 	{
 		path: '/scoreboard',
