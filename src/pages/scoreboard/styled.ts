f167fd2c82596eb5fe3c628a34e2ce6e6cac48b8
romalebedev <54033038+romalebedev@users.noreply.github.com>
2021-10-04 19:53:10 +0300

    add scoreboard (#8)
    Co-authored-by: rklebede <RLebedev@stream.ru>

---
+++ src/pages/scoreboard/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
+	width: 40%;
+`;
+
+// TODO del after merge and use ui Wrapper
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
