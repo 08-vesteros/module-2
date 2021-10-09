a61a8e95dd1c809899a92d6649c675db49d0bc76
Sergei Zaderaka <komrad.zaderaka@ya.ru>
2021-10-09 22:12:58 +0300

    S5T12 port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 app.use(express.static('dist'));
 app.get('*', (_req, res) => {
 	res.sendFile(path.join(__dirname, '/dist/index.html'));
