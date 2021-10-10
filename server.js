79e3cea2a032020f6b3ecc514f5d3b35cb67cfd0
Sergei Zaderaka <komrad.zaderaka@ya.ru>
2021-10-10 13:29:52 +0300

    S5T12 procfile

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3002;
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('dist'));
-	app.get('*', (_req, res) => {
-		res.sendFile(path.join(__dirname, '/dist/index.html'));
-	});
-}
+app.use(express.static('dist'));
+app.get('*', (_req, res) => {
+	res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
